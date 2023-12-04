87593d0abdad4fc3233100b316c06b8fe7e77ad9
Siddartha <siddartharedddyl@gmail.com>
2023-12-04 17:41:34 +0530

    done part2-reflog

--- part2-reflog/src/App.jsx
+++ part2-reflog/src/App.jsx
@@ -3,9 +3,16 @@ import { useState, useEffect } from "react";
 import serv from "./services/notes";
 
 const App = () => {
+  
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
   const [notes, setNotes] = useState([]);
   const [newNotes, setNewNotes] = useState("enter please");
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
   useEffect(() => {
     console.log("useEffect");
     serv.getAll().then((response) => {
@@ -40,23 +47,28 @@ const App = () => {
   const handleImportance = (id) => {
     const note = notes.find((e) => e.id === id);
     const changedNote = { ...note, important: !note.important };
-    serv.update(id, changedNote).then((response) => {
-      console.log("sidd", response);
-      setNotes(notes.map((n) => (n.id !== id ? n : response)));
-      console.log(`importance of ${id} needs to be toggled`);
-    }).catch(error => {
-      alert(
-        `the note '${note.content}' was already deleted from server`
-      )
-      setNotes(notes.filter(n => n.id !== id))
-    });
+    serv
+      .update(id, changedNote)
+      .then((response) => {
+        console.log("sidd", response);
+        setNotes(notes.map((n) => (n.id !== id ? n : response)));
+        console.log(`importance of ${id} needs to be toggled`);
+      })
+      .catch((error) => {
+        setErrorMessage(
+          `error message man becoz${note.content} is already deleted`
+        );
+        setTimeout(()=>setErrorMessage(null), 5000);
+        setNotes(notes.filter((n) => n.id !== id));
+      });
   };
   const notesToShow = showAll
     ? notes
     : notes.filter((item) => item.important === true);
   return (
-    <div>
+    <div style={footerStyle}>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <ul>
         {notesToShow.map((note, id) => (
           <Note
@@ -76,5 +88,11 @@ const App = () => {
     </div>
   );
 };
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null;
+  }
 
+  return <div className="error">{message}</div>;
+};
 export default App;
--- part2-reflog/src/components/Note.jsx
+++ part2-reflog/src/components/Note.jsx
@@ -1,7 +1,7 @@
 const Note = ({ note, toggleImportance }) => {
   const label = note.important ? "make not important" : "make it imporatant";
   return (
-    <li>
+    <li className="note">
       {note.content}
       <button onClick={toggleImportance}>{label}</button>
     </li>
