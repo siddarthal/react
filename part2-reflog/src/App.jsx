573b2382a1f16d6b395d622039ead6843a431a3d
Siddartha <siddartharedddyl@gmail.com>
2023-12-04 15:00:27 +0530

    server js update

--- part2-reflog/src/App.jsx
+++ part2-reflog/src/App.jsx
@@ -1,7 +1,6 @@
 import Note from "./components/Note";
 import { useState, useEffect } from "react";
-import axios from "axios";
-import noteService from './services/notes';
+import serv from "./services/notes";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
@@ -9,10 +8,10 @@ const App = () => {
   const [showAll, setShowAll] = useState(true);
   useEffect(() => {
     console.log("useEffect");
-    noteService.getAll().then((response) => {
+    serv.getAll().then((response) => {
       console.log("promise fulfilled");
-      const data = response.data;
-      setNotes(data);
+      console.log(response);
+      setNotes(response);
     });
   }, []);
   console.log("render", notes.length, "notes");
@@ -23,12 +22,11 @@ const App = () => {
       important: Math.random() < 0.5,
     };
     console.log(noteObject);
-   noteService.create(noteObject).then((response) => {
+    serv.create(noteObject).then((response) => {
       console.log(response);
-      setNotes(notes.concat(response.data));
+      setNotes(notes.concat(response));
       setNewNotes("");
     });
-    
   };
   const handleChange = (e) => {
     console.log(e.target);
@@ -42,11 +40,11 @@ const App = () => {
   const handleImportance = (id) => {
     const note = notes.find((e) => e.id === id);
     const changedNote = { ...note, important: !note.important };
-    // setNotes([...notes,changedNote]); this wont work
-    noteService.update.put(id,changedNote).then((response) => {
-      setNotes(notes.map((n) => (n.id !== id ? n : response.data)));
+    serv.update(id, changedNote).then((response) => {
+      console.log("sidd", response);
+      setNotes(notes.map((n) => (n.id !== id ? n : response)));
+      console.log(`importance of ${id} needs to be toggled`);
     });
-    console.log(`importance of ${id} needs to be toggled`);
   };
   const notesToShow = showAll
     ? notes
@@ -55,9 +53,9 @@ const App = () => {
     <div>
       <h1>Notes</h1>
       <ul>
-        {notesToShow.map((note) => (
+        {notesToShow.map((note, id) => (
           <Note
-            key={note.id}
+            key={id}
             note={note}
             toggleImportance={() => handleImportance(note.id)}
           />
